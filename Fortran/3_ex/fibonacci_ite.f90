83d028da33682fc05d53d9da6c83519841275878
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-07-05 03:10:35 -0300

    Add changes

---
+++ Fortran/3_ex/fibonacci_ite.f90
@@ -0,0 +1,62 @@
+program fibonacci_ite
+    implicit none
+    
+    INTEGER :: A,B,S1,S2,i
+    DOUBLE PRECISION :: start1, finish1, start2, finish2
+    INTEGER :: C(0:1) !Defining an array ----> do not forget
+
+
+    print*, 'Fibonacci s indice value for n = 50'
+    write(*,'(/A)')
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    !Variable iterative fibonacci  --> Worse performance, why?
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    A = 0 
+    B = 1
+    S1 = 0
+
+    call cpu_time(start1)
+    do i = 1,45
+
+        A = S1
+        S1 = A + B
+        B = A
+
+    end do 
+    call cpu_time(finish1)
+
+    print*,'Variable iterative fibonacci'
+    print*, 'The number is: ',S1
+    print '("Elapsed time is: ",f6.5," seconds")', finish1 - start1
+
+    write(*,'(/A)')
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    !Array iterative fibonacci  --> Worse performance, why?
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    C(0) = 0
+    C(1) = 1
+
+    S2 = 0
+    
+    !print*, C !--> Just to be more clear
+
+    call cpu_time(start2)
+    do i = 1,10
+
+        C(0) = S2
+        S2 = sum(C)
+        C(1) = C(0)
+
+    end do
+    call cpu_time(finish2)
+
+    print*,'Array iterative fibonacci'
+    print*, 'The number is: ',S2
+    print *, finish2 - start2
+    
+
+end program fibonacci_ite
---
+++ Fortran/3_ex/fibonacci_recur.f90
@@ -0,0 +1,31 @@
+program fibonacci_recur
+    implicit none
+    INTEGER:: n1
+    real :: start, finish
+
+
+    call cpu_time(start)
+    n1 = fibonacci_recursive(10)
+    call cpu_time(finish)
+
+
+    print*, 'The number is: ',n1
+    print*, 'Elapsed time is: ', finish - start
+
+
+    contains !Separate the program from the function or sub-routine
+
+    recursive function fibonacci_recursive(n) result(fbn) !
+        integer, intent(in) :: n !Intent (in) that is the variable recieves info from outside the function
+        integer :: fbn
+
+        select case (n)
+
+            case (:2);      fbn = 1
+            case default;   fbn = fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+        end select
+    end function fibonacci_recursive
+
+end program fibonacci_recur
+
