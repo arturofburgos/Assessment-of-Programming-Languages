51daf7182aaf9b1f5570dbed4dedadf80e7d14c9
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-07-28 15:02:46 -0300

    Add changes

--- 1D Linear Convection/Python/1D_linear_convection.py
+++ 1D Linear Convection/Python/1D_linear_convection.py
@@ -9,15 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 # First define the:
 
 X = 5 # Length of the grid
-nx = 81 # Number of nodes
+nx = 41 # Number of nodes
 dx = X/(nx-1) # The distance between consecutive nodes
 
-nt = 100 # The elapsed time
-dt = 0.025 # The amount of time each timestep cover
+nt = 50 # The number of timesteps I want to calculate
+dt = 0.05 # The amount of time each timestep cover
 
 c = 1 # Wavespeed
 
@@ -28,13 +27,25 @@ u[int(0.5/dx):int(1/dx+1)] = 2
 
 un = np.ones(nx)
 
-plt.plot(np.linspace(0,2,nx),u)
-plt.show() 
 
-for n in range(nt):
-    un = u.copy()
+fig , ax = plt.subplots(figsize=(10 , 7),nrows=2)
+
+ax[0].set_title('Wave velocity through time passing in a 1-D grid ')
+ax[1].set_title('Comparing the wave in first and the last timestep')
+ax[0].plot(np.linspace(0,5,nx),u)
+ax[1].plot(np.linspace(0,5,nx),u)
+
+for n in range(1,nt):
+    un[:] = u[:]
+    #un = u.copy()
+
     for i in range(1,nx):
         u[i] = un[i] - c * (dt/dx) * (un[i]-un[i-1])
+    
+    ax[0].plot(np.linspace(0,5,nx),u) # This shows what happens through time
 
-plt.plot(np.linspace(0,5,nx),u)
-plt.show()    
+ax[1].plot(np.linspace(0,5,nx),u) 
+plt.show()
+
+print('The velocity last timestep velocity values are: ')
+print(u)    
