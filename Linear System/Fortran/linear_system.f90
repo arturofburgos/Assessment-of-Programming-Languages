cd60dfe8cd754d76f0464b9a02f52e70a0a9f19f
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-08-26 10:22:18 -0300

    Add changes

---
+++ Linear System/Fortran/linear_system.f90
@@ -0,0 +1,23 @@
+program linear_system
+
+    !Undergraduate Student: Arturo Burgos
+    !Professor: João Rodrigo Andrade
+    !Federal University of Uberlândia - UFU, Fluid Mechanics Laboratory
+    !MFLab, Block 5P, Uberlândia, MG, Brazil
+
+    !Fourth exercise: Solving a Linear System --> ax = b
+
+
+    implicit none
+    
+
+
+
+
+
+
+
+
+
+
+end program linear_system
