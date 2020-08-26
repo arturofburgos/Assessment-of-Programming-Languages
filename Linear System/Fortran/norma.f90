26f2583108729123a01c9bd5cfea87090bf0f91c
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-08-26 09:37:55 -0300

    Add changes

--- Linear System/Fortran/norma.f90
+++ Linear System/Fortran/norma.f90
@@ -1,15 +1,32 @@
 program norma
+
         implicit none
         
-        INTEGER :: i
-        INTEGER, DIMENSION(:), ALLOCATABLE :: a
+        real :: b
+        real, DIMENSION(:), ALLOCATABLE :: numbers
 
-        ALLOCATE (a(2))
+        numbers = (/1.5, 54.236879,12.5,0.9,7.2 /)
 
-        a(1) = 3
-        a(2) = 7
-
-        print *, maxval(a)
+        b = normainf(numbers)
+        
+        write(*,2) b
+        2 format(1f9.6)
+        
+    
+        contains
+        
+        function normainf(vec) result(y)
+    
+            implicit none
+    
+            
+            real, DIMENSION(:), ALLOCATABLE :: vec
+            real :: y
+           
+            
+            y = maxval(ABS(vec))
+    
+        end function normainf 
 
 
 end program norma
