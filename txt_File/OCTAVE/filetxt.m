095300b0a99f171bb615ad7574f2e4d3f219ed57
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-09-09 23:23:58 -0300

    Add changes

---
+++ txt_File/OCTAVE/filetxt.m
@@ -0,0 +1,26 @@
+# Undergraduate Student: Arturo Burgos
+# Professor: João Rodrigo Andrade
+# Federal University of Uberlândia - UFU, Fluid Mechanics Laboratory
+# MFLab, Block 5P, Uberlândia, MG, Brazil
+
+# First exercise: Consider an arbitrary Matrix NxNx3 - by a common loop
+
+clear;
+clc;
+
+x = 1:10:100;
+
+y = cos(x);
+tic
+fid = fopen("/home/arturo/Desktop/IC/Comparing-Languages/txt_File/OCTAVE/...
+test.txt","w");
+for i = 1:length(x)
+  fprintf(fid, "%d \t %d\n", x(i),y(i));
+  
+endfor
+fdisp(fid,"\n");
+for i = 1:length(y)
+  fprintf(fid, "%d ", y(i));
+endfor
+fclose(fid);
+toc
