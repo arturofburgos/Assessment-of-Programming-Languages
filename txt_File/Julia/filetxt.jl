50adeb43bc143a0e2e59f132f3efa609056c5235
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-09-10 20:46:16 -0300

    Add changes

---
+++ txt_File/Julia/filetxt.jl
@@ -0,0 +1,21 @@
+# Undergraduate Student: Arturo Burgos
+# Professor: João Rodrigo Andrade
+# Federal University of Uberlândia - UFU, Fluid Mechanics Laboratory - MFLab, Block 5P, Uberlândia, MG, Brazil
+
+
+# Fifth exercise: txt File 
+
+
+using Printf # Just the same as import a module
+using Statistics
+using CPUTime
+
+x = collect(1:10:100)
+
+y = cos.(x)
+
+println(x)
+
+println(y)
+
+
